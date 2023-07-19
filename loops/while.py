725b764767335ca6fe4dc5ee9405fbebc9835293
h1y1d1 <varmavegasana#gmail.com>
2023-07-19 11:57:20 -0400

    while loop

---
+++ loops/while.py
@@ -0,0 +1,66 @@
+"""i = 1
+while i < 6:
+    print(i)
+    i += 1 # i = i + 1(old form)"""
+
+"""username = input("enter username:")
+password = input("enter password:")
+
+while username!="surya" and password != "1111":
+    username = input("enter username:")
+    password = input("enter password:")"""
+
+"""num = int(input("enter number: "))
+total = 0
+while num!=0:
+    total+num
+    print(num)
+    num += int(input("enter number: "))
+    
+print(num)"""
+
+"""marks = int(input("enter your marks: "))
+
+while marks<35:
+    print("try again in next sem")
+    marks = int(input("enter your marks: "))
+else:
+    print("passssss")"""
+
+#loop 1. statement -- break, continue, pass
+
+"""marks = int(input("enter your marks: "))
+count = 1
+while marks<35:
+    if count<3:         
+        print("try again in next sem")
+        count += 1   
+        marks = int(input("enter your marks: "))
+    else:
+        print("out of attempts")
+        break
+else:
+    print("passssss")"""
+
+#prime numbers...100 add and get sum...
+"""num = 1
+while num<51:
+    if num%5==0:
+        print("busssss")
+        num+=1
+    else:
+        print(num)
+        num+=1"""
+
+"""circle = int(input("enter your circle radius: "))
+count = 1
+while circle !=0:
+    area = (22/7)*(circle**2)
+    print("the area of ",count,"circle is:",area)
+    count+=1
+    circle = int(input("enter your circle radius: "))"""
+
+
+
+
+
