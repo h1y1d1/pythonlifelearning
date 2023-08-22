175cee990ea2582c67f8b58fbcd2a4b1bbc9d0e5
h1y1d1 <varmavegasana@gmail.com>
2023-08-22 17:18:03 -0400

    project

--- projects/railwayticket.py
+++ projects/railwayticket.py
@@ -1,12 +1,25 @@
-class Account():
+class Account:
     def __init__(self,username,password):
         self.username = username
         self.password = password
     def checkpassword(self,password):
         return self.password == password
+    
+class traininfo:
+    def __init__(self,train_num,destination,avl_seats,source):
+        self.train_num = train_num
+        self.destination = destination
+        self.avl_seats = avl_seats
+        self.source = source
+    def displayinfo(self):
+        print(f"train_num {self.train_num}")
+        print(f"destination {self.destination}")
+        print(f"avl_seats {self.avl_seats}")
+        print(f"source {self.source}")
+        
         
 accounts = [
-    #Account("","")
+    Account("user1","password")
 ]
 loginaccount = None
 while True:
@@ -32,5 +45,35 @@ while True:
             break
     else:
         print("enter valid details")
+#print("search ur destination")
+if loginaccount is not None:
+    trains=[traininfo("12345","hyderabad","130","vijayawada"),
+            traininfo("12345","vja","110","vizag")]
+    for train in trains:
+        train.displayinfo()
+
+
+
+    # train_details = []
+    # print("1. Vizag:\n 2. Vijayawada:\n 3. hyderabad:\n")
+    # destiny = int(input("choose ur option: "))
+    # if destiny == 1:
+    #     train_num = "12345"
+    #     destination = "Vijayawada"
+    #     avl_seats = "120"
+    #     print("train_num",train_num,"destination",destination,"avl_seats",avl_seats)
+    # elif destiny==2:
+    #     train_num = "67891"
+    #     destination = "Vizag"
+    #     avl_seats = "115"
+    #     print("train_num",train_num,"destination",destination,"avl_seats",avl_seats)
+    # elif destiny==3:
+    #     train_num = "78901"
+    #     destination = "Hyderabad"
+    #     avl_seats = "120"
+    #     print("train_num",train_num,"destination",destination,"avl_seats",avl_seats)
+    # else:
+    #     print("Please select ur destination")
+
 
     
